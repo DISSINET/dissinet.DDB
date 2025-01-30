18cd59930b817bc9950c0012dd82a3d7ead4f113
Ptrhnk <petr.nahak@gmail.com>
2025-01-30 12:28:50 +0000

    use bcryptjs instead of bcrypt on server

--- packages/server/src/common/auth.ts
+++ packages/server/src/common/auth.ts
@@ -1,4 +1,4 @@
-import * as bcrypt from "bcrypt";
+import * as bcrypt from "bcryptjs";
 import { sign as signJwt } from "jsonwebtoken";
 import { IUser } from "@shared/types/user";
 import { expressjwt, Request as JWTRequest } from "express-jwt";
